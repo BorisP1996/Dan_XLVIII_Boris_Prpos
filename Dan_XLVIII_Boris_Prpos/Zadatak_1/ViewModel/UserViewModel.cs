02e397588c9e541caed2050980a3cb9a6f65f36b
BorisP1996 <prposboris13@hotmail.com>
2020-08-03 14:04:13 +0200

    display order status after login

--- Dan_XLVIII_Boris_Prpos/Zadatak_1/ViewModel/UserViewModel.cs
+++ Dan_XLVIII_Boris_Prpos/Zadatak_1/ViewModel/UserViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -27,6 +28,34 @@ namespace Zadatak_1.ViewModel
             tblorder.CustomerJMBG = username;
             //filling list with data from database
             OrderList = GetOrders();
+            Show();
+        }
+        
+        public void Show()
+        {         
+            List<string> dateAndStatus = new List<string>();
+            List<tblOrder> tblOrders = context.tblOrders.ToList();
+
+            foreach (var item in tblOrders)
+            {
+                if (item.CustomerJMBG==tblOrder.CustomerJMBG)
+                {
+                    dateAndStatus.Add(item.OrderDate + " " + item.OrderStatus+ "\n");
+                }
+            }
+
+            string total = " ";
+
+            foreach (string item in dateAndStatus)
+            {
+                total += item + " ";
+            }
+            total += "\nThis message box will be closed automaticaly after 2s";
+
+            if (dateAndStatus.Count!=0)
+            {
+                AutoClosingMessageBox.Show(total, "Orders status", 2000);
+            }
         }
 
         public UserViewModel(User user)
@@ -266,5 +295,33 @@ namespace Zadatak_1.ViewModel
             }
             return ByUser;
         }
+    }
+    public class AutoClosingMessageBox
+    {
+        System.Threading.Timer _timeoutTimer;
+        string _caption;
+        AutoClosingMessageBox(string text, string caption, int timeout)
+        {
+            _caption = caption;
+            _timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                null, timeout, System.Threading.Timeout.Infinite);
+            MessageBox.Show(text, caption);
+        }
+        public static void Show(string text, string caption, int timeout)
+        {
+            new AutoClosingMessageBox(text, caption, timeout);
+        }
+        void OnTimerElapsed(object state)
+        {
+            IntPtr mbWnd = FindWindow(null, _caption);
+            if (mbWnd != IntPtr.Zero)
+                SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            _timeoutTimer.Dispose();
+        }
+        const int WM_CLOSE = 0x0010;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
     }
 }
